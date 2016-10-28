4dba163dca2ae2ed4001d5331741d52f87bdd9da
robertohonda <roberto_junior_honda@hotmail.com>
2016-10-28 20:07:24 -0300

    Add files via upload

--- rascunho.cpp
+++ rascunho.cpp
@@ -5,7 +5,7 @@
 #include<time.h>
 #include<SDL.h>
 
-#define TAM 20
+#define TAM 10
 
 typedef struct{
     int x;
@@ -30,6 +30,7 @@ void gera_semente();
 
 int SDL_main(int argc, char* args[])
 {
+
     fernando.corpo[0].x = 0;
     fernando.corpo[0].y = 0;
     fernando.tamanho = 1;
@@ -42,9 +43,82 @@ int SDL_main(int argc, char* args[])
     imprime();
     while(true)
     {
+        int flag = 0;
+        char dir;
+
         system("cls");
         gera_semente();
         imprime();
+
+        scanf("\n%c", &fernando.direcao);
+        Posicao curva = fernando.corpo[0];
+        switch(fernando.direcao)
+        {
+            case 'd':
+                while(flag==0)///ate colidir com comida
+                {
+                    int tam = fernando.tamanho;
+                    for(int i=0;i<fernando.tamanho;i++)///andar
+                    {
+                        mat[fernando.corpo[tam-1].x][fernando.corpo[tam-1].y] = ' ';
+                        fernando.corpo[i].y++;
+                        if(fernando.corpo[i].x<curva.x)
+                            fernando.corpo[i].x++;
+                        else if(fernando.corpo[i].x>curva.x)
+                            fernando.corpo[i].x--;
+                    }
+                    if(mat[fernando.corpo[0].x][fernando.corpo[0].y] == '*')///colisao
+                    {
+                        fernando.corpo[tam].x = fernando.corpo[tam-1].x;
+                        fernando.corpo[tam].y = fernando.corpo[tam-1].y-1;
+                        fernando.tamanho++;
+                        flag = 1;
+                        //imprime();
+                    }
+                    //system("pause");
+                    system("cls");
+                    imprime();
+                    Sleep(100);
+                }
+                break;
+            case 's':
+                while(flag==0)///ate colidir com comida
+                {
+                    int tam = fernando.tamanho;
+                    for(int i=0;i<fernando.tamanho;i++)///andar
+                    {
+                        mat[fernando.corpo[tam-1].x][fernando.corpo[tam-1].y] = ' ';
+                        fernando.corpo[i].x++;
+                        if(fernando.corpo[i].y<curva.y)
+                        {
+                            mat[fernando.corpo[tam-1].x][fernando.corpo[tam-1].y] = ' ';
+                            fernando.corpo[i].y++;
+                            fernando.corpo[i].x++;
+                        }
+                        else if(fernando.corpo[i].y>curva.y)
+                        {
+                            mat[fernando.corpo[tam-1].x][fernando.corpo[tam-1].y] = ' ';
+                            fernando.corpo[i].y--;
+                        }
+                        system("pause");
+                    }
+                    if(mat[fernando.corpo[0].x][fernando.corpo[0].y] == '*')///colisao
+                    {
+                        fernando.corpo[tam].x = fernando.corpo[tam-1].x-1;
+                        fernando.corpo[tam].y = fernando.corpo[tam-1].y;
+                        fernando.tamanho++;
+                        flag = 1;
+                        //imprime();
+                    }
+                    //system("pause");
+                    system("cls");
+                    imprime();
+                    Sleep(100);
+                }
+                break;
+        }
+
+        /*
         while(SDL_PollEvent(&evento)!=0);
         SDL_WaitEventTimeout(&evento, INFINITE);
         if(evento.type==SDL_KEYDOWN)
@@ -69,7 +143,8 @@ int SDL_main(int argc, char* args[])
             }
         }
         while(SDL_PollEvent(&evento)!=0);
-        Sleep(1000);
+        */
+        //Sleep(1000);
     }
 
     return 0;
@@ -85,7 +160,8 @@ void inicializa()
 void imprime()
 {
     for(int i=0;i<fernando.tamanho;i++)
-        mat[fernando.corpo[i].x][fernando.corpo[i].y] = '.';
+        mat[fernando.corpo[i].x][fernando.corpo[i].y] = 220;
+
     for(int i=0;i<TAM;i++)
     {
         for(int j=0;j<TAM;j++)
@@ -100,7 +176,7 @@ void gera_semente()
     srand(time(NULL));
     while(true)
     {
-        x = rand()%TAM;
+        x = rand()%TAM/2;
         y = rand()%TAM;
         if(mat[x][y]==' ')
         {
